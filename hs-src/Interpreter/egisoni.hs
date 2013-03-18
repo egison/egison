4c079d4fd1c9b1b5cddbbfb152ff6f94dae4ee57
linerlock <x.linerlock@gmail.com>
2013-03-18 17:03:20 +0900

    update Interpreter

--- hs-src/Interpreter/egisoni.hs
+++ hs-src/Interpreter/egisoni.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Applicative ((<$>), (<*>))
 import Control.Monad.Error
 
 import Data.ByteString.Lazy (ByteString)
@@ -7,10 +8,10 @@ import Data.ByteString.Lazy.Char8 ()
 import qualified Data.ByteString.Lazy.Char8 as B
 import Text.Parsec
 import Text.Parsec.ByteString.Lazy
+import Text.Regex.Posix
 
 import System.Environment
 import System.Console.Haskeline
-
 import Language.Egison.Types
 import Language.Egison.Parser
 import Language.Egison.Core
@@ -27,6 +28,7 @@ main = do args <- getArgs
                 evalTopExprs env exprs
               either print return result
 
+
 runParser' :: Parser a -> String -> Either EgisonError a
 runParser' parser input = either (throwError . Parser) return $ parse parser "egison" (B.pack input)
 
@@ -35,16 +37,41 @@ runEgisonTopExpr env input = runErrorT . runEgisonM $ do
   expr <- liftError $ runParser' parseTopExpr input
   evalTopExpr env expr
 
-repl :: IO ()
-repl = primitiveEnv >>= repl'
+runEgisonTopExprs :: Env -> String -> IO (Either EgisonError ())
+runEgisonTopExprs env input = runErrorT . runEgisonM $ do 
+  expr <- liftError $ runParser' parseTopExprs input
+  evalTopExprs env expr
 
-repl' :: Env -> IO ()
-repl' env = do
-  mInput <- runInputT defaultSettings $ getInputLine "> "
-  case mInput of
-    Nothing -> return ()
-    Just input -> 
-      do result <- runEgisonTopExpr env input
-         case result of
-           Left err -> print err >> repl' env
-           Right env -> repl' env
+getInputLine' :: MonadException m => String -> InputT m (Maybe String)
+getInputLine' s = do
+  input <- getInputLine s
+  cont <- getInputLine' ".."
+  return $ (++) <$> input <*> cont
+      
+
+repl :: IO ()
+repl = primitiveEnv >>= flip repl' "> "
+
+repl' :: Env -> String -> IO ()
+repl' env prompt = loop env prompt ""
+  where
+    loop :: Env -> String -> String -> IO ()
+    loop env prompt' rest = do
+      input <- runInputT defaultSettings $ getInputLine prompt'
+      case input of
+        Nothing -> return ()
+        Just "" ->  loop env prompt ""
+        Just input' -> do
+          let newInput = rest ++ input'
+          result <- runEgisonTopExpr env newInput
+          case result of
+            Left err | show err =~ "unexpected end of input" -> do
+              loop env (take (length prompt) (repeat '.')) $ newInput ++ "\n"
+            Left err -> do
+              liftIO $ putStrLn $ show err
+              loop env prompt ""
+            Right _ ->
+              loop env prompt ""
+        
+     
+    
