176f10686138c86c6c7c6e7311a989172b83dccb
linerlock <x.linerlock@gmail.com>
2013-03-25 14:04:16 +0900

    update

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -4,34 +4,15 @@ import Control.Applicative ((<$>), (<*>))
 import Control.Monad.Error
 import Control.Applicative
 import Control.DeepSeq (NFData(rnf))
-import Language.Egison.Types
-import Language.Egison.Core
-import Language.Egison.Primitives
+import Language.Egison
 import Criterion.Main
 import Text.Parsec
 import Text.Parsec.ByteString
 
-loadLibraries :: Env -> IO Env
-loadLibraries env = do
-  result <- runErrorT $ runEgisonM $ foldM evalTopExpr env (map Load libraries)
-  case result of
-    Left err -> do
-      print . show $ err
-      return env
-    Right env' -> 
-      return env'
-  where
-    libraries :: [String]
-    libraries = [ "lib/core/base.egi"
-                , "lib/core/number.egi"
-                , "lib/core/collection.egi"
-                , "lib/core/pattern.egi" ]
-
-runEgisonFile :: String -> IO ()
+runEgisonFile :: String -> IO (Either EgisonError Env)
 runEgisonFile path = do
-  env <- primitiveEnv >>= loadLibraries
-  result <- runErrorT . runEgisonM $ evalTopExpr env (LoadFile path)
-  either print (const $ return ()) $ result
+  env <- loadPrimitives nullEnv >>= loadLibraries
+  runEgisonM $ evalTopExpr env (LoadFile path) 
 
 main :: IO ()
 main = defaultMain [
