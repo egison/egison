66152e3f2df72b7b94aae5c0c9f0b0e649c5aa1d
alpicola <ryo@alpico.la>
2013-03-25 19:27:19 +0900

    add test cases

--- test/UnitTest.hs
+++ test/UnitTest.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Control.Monad
+
 import Test.Framework (defaultMain)
 import Test.Framework.Providers.HUnit (hUnitTestToTests)
 import Test.HUnit
@@ -18,5 +20,17 @@ testCases = [ "test/collection-test.egi"
 runTestCase :: FilePath -> Test
 runTestCase file = TestLabel file . TestCase $ do 
   env <- primitiveEnv >>= loadLibraries
-  result <- runEgisonM $ loadFile file >>= evalTopExprs env
+  result <- runEgisonM $ loadFile file >>= evalTopExprs' env
   assertString $ either show (const "") result
+
+evalTopExprs' :: Env -> [EgisonTopExpr] -> EgisonM ()
+evalTopExprs' env exprs = do
+  let (bindings, tests) = foldr collectDefsAndTests ([], []) exprs
+  env <- recursiveBind env bindings
+  forM_ tests $ evalExpr' env
+ where
+  collectDefsAndTests (Define name expr) (bindings, tests) =
+    ((name, expr) : bindings, tests)
+  collectDefsAndTests (Test expr) (bindings, tests) =
+    (bindings, expr : tests)
+  collectDefsAndTests _ r = r
