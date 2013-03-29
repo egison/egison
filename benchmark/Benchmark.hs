8b683258f16204cce53623e49d9f8c75d7125a4e
linerlock <x.linerlock@gmail.com>
2013-03-29 17:01:05 +0900

    change to 3 the number of samples of the benchmark

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -4,18 +4,24 @@ import Control.Applicative ((<$>), (<*>))
 import Control.Monad.Error
 import Control.Applicative
 import Control.DeepSeq (NFData(rnf))
-import Language.Egison
 import Criterion.Main
+import Criterion.Config
 import Text.Parsec
 import Text.Parsec.ByteString
+
+import Language.Egison
 
 runEgisonFile :: String -> IO (Either EgisonError Env)
 runEgisonFile path = do
   env <- loadPrimitives nullEnv >>= loadLibraries
   evalEgisonTopExpr env $ LoadFile path
 
+config :: Config
+config = defaultConfig { cfgSamples   = ljust 3 
+                       , cfgPerformGC = ljust True }
+
 main :: IO ()
-main = defaultMain [
+main = defaultMainWith config (return ()) [
     bgroup "fact" [ bench "30000" $ runEgisonFile "benchmark/fact-30000.egi" ]
   ]
 
