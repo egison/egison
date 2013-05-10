6728a241d5a6f1d99b2a3c4e4e2355301033a78e
xenophobia <xeno.lambda@i.softbank.jp>
2013-05-10 18:43:31 +0900

    add benchmark files of {cons, snoc} pattern of list matcher

---
+++ benchmark/Benchmark-Collection.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Main where
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad.Error
+import Control.Applicative
+import Control.DeepSeq (NFData(rnf))
+import Criterion.Main
+import Criterion.Config
+import Text.Parsec
+import Text.Parsec.ByteString
+
+import Language.Egison
+
+runEgisonFile :: String -> IO (Either EgisonError Env)
+runEgisonFile path = do
+  env <- loadPrimitives nullEnv >>= loadLibraries
+  evalEgisonTopExpr env $ LoadFile path
+
+config :: Config
+config = defaultConfig { cfgSamples   = ljust 3 
+                       , cfgPerformGC = ljust True }
+
+main :: IO ()
+main = defaultMainWith config (return ()) [
+    bgroup "collection" [ bench "cons-bench" $ runEgisonFile "benchmark/collection-bench-cons.egi"
+                        , bench "snoc-bench" $ runEgisonFile "benchmark/collection-bench-snoc.egi"
+                        ]
+  ]
+
