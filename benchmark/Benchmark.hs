6181e1cc03ad19598387a0c0d7a0ebf6f42fb84a
linerlock <x.linerlock@gmail.com>
2013-05-28 15:00:06 +0900

    merge Benchmark-Collection into Benchmark

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -6,22 +6,22 @@ import Control.Applicative
 import Control.DeepSeq (NFData(rnf))
 import Criterion.Main
 import Criterion.Config
-import Text.Parsec
-import Text.Parsec.ByteString
-
 import Language.Egison
 
 runEgisonFile :: String -> IO (Either EgisonError Env)
-runEgisonFile path = do
-  env <- loadPrimitives nullEnv >>= loadLibraries
-  evalEgisonTopExpr env $ LoadFile path
+runEgisonFile path = loadPrimitives nullEnv >>= loadLibraries >>= flip loadEgisonFile path
 
 config :: Config
 config = defaultConfig { cfgSamples   = ljust 3 
                        , cfgPerformGC = ljust True }
 
 main :: IO ()
-main = defaultMainWith config (return ()) [
-    bgroup "fact" [ bench "30000" $ runEgisonFile "benchmark/fact-30000.egi" ]
-  ]
+main = defaultMainWith config (return ()) 
+       [ bgroup "fact" [ bench "30000" $ runEgisonFile "benchmark/fact-30000.egi" ]
+       , bgroup "collection"  
+         [ bench "cons-bench" $ runEgisonFile "benchmark/collection-bench-cons.egi"
+         , bench "cons-bench-large" $ runEgisonFile "benchmark/collection-bench-cons-large.egi"
+         , bench "snoc-bench" $ runEgisonFile "benchmark/collection-bench-snoc.egi"
+         ]
+       ]
 
