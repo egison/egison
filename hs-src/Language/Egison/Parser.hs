472f93eddc5be80d25868a9608bfda3523e7449f
Satoshi Egi <egi@egison.org>
2014-07-29 21:26:53 +0900

    update do expression

--- hs-src/Language/Egison/Parser.hs
+++ hs-src/Language/Egison/Parser.hs
@@ -315,7 +315,7 @@ letExpr :: Parser EgisonExpr
 letExpr = keywordLet >> LetExpr <$> bindings <*> expr
 
 doExpr :: Parser EgisonExpr
-doExpr = keywordDo >> DoExpr <$> statements <*> option (TupleExpr []) expr
+doExpr = keywordDo >> DoExpr <$> statements <*> option (ApplyExpr (VarExpr "return") (TupleExpr [])) expr
 
 statements :: Parser [BindingExpr]
 statements = braces $ sepEndBy statement whiteSpace
