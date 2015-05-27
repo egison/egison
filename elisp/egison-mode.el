25f49eaaeac55a8b716a96f4e6cb4098596ef01a
p <_>
2015-05-27 18:03:56 +0900

    fix the header format

--- elisp/egison-mode.el
+++ elisp/egison-mode.el
@@ -1,12 +1,39 @@
 ;;; egison-mode.el --- Egison editing mode
 
-;; Copyright 2011-2013 Satoshi Egi
+;; Copyright (C) 2011-2015 Satoshi Egi
+
+;; Permission is hereby granted, free of charge, to any person obtaining
+;; a copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the Software
+;; is furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included
+;; in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+;; INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+;; A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+;; CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+;; OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ;;; Author: Satoshi Egi <egisatoshi@gmail.com>
 ;;; URL: https://github.com/egisatoshi/egison3/blob/master/elisp/egison-mode.el
 ;;; Version: 0.1.5
 
-;; Code goes here
+;;; Commentary:
+
+;; Emacs Mode for Egison
+;;
+;; Please put it in your load-path of Emacs. Then, add the following
+;; lines in your .emacs.
+;;
+;;   (autoload 'egison-mode "egison-mode" "Major mode for editing Egison code." t)
+;;   (setq auto-mode-alist (cons `("\\.egi$" . egison-mode) auto-mode-alist))
+
+;;; Code:
 
 (defconst egison-font-lock-keywords-1
   (eval-when-compile
