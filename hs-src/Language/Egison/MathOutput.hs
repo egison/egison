c4423a064fe1698c5be2591531eaab6f6d2ab78e
Satoshi Egi <egi@egison.org>
2017-12-22 09:37:27 +0900

    fix

--- hs-src/Language/Egison/MathOutput.hs
+++ hs-src/Language/Egison/MathOutput.hs
@@ -72,7 +72,7 @@ showMathExprAsciiMath (Tensor lvs mis)
   | otherwise = "(" ++ showMathExprAsciiMathArg lvs ++ ")_(" ++ showMathExprAsciiMathIndices (filter isSub mis) ++ ")^(" ++ showMathExprAsciiMathIndices (filter (not . isSub) mis) ++ ")"
 showMathExprAsciiMath (Tuple lvs) = "(" ++ showMathExprAsciiMathArg lvs ++ ")"
 showMathExprAsciiMath (Collection lvs) = "{" ++ showMathExprAsciiMathArg lvs ++ "}"
-showMathExprAsciiMath (Exp x) = "e^" ++ showMathExprAsciiMath x
+showMathExprAsciiMath (Exp x) = "e^(" ++ showMathExprAsciiMath x ++ ")"
 
 isSub :: MathIndex -> Bool
 isSub x = case x of
@@ -124,7 +124,7 @@ showMathExprLatex (Tensor lvs mis) = case head lvs of
                                        _ -> "\\begin{pmatrix} " ++ showMathExprLatexVectors lvs ++ "\\end{pmatrix}" ++ showMathExprLatexScript mis
 showMathExprLatex (Tuple lvs) = "(" ++ showMathExprLatexArg lvs ", " ++ ")"
 showMathExprLatex (Collection lvs) = "{" ++ showMathExprLatexArg lvs ", " ++ "}"
-showMathExprLatex (Exp x) = "e^" ++ showMathExprLatex x
+showMathExprLatex (Exp x) = "e^{" ++ showMathExprLatex x ++ "}"
 
 showMathExprLatex' :: MathExpr -> String
 showMathExprLatex' (Plus lvs) = "(" ++ showMathExprLatex (Plus lvs) ++ ")"
