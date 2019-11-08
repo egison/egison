654d8660b0a31217e24b0ed7928f83f5fe7eebd8
momohatt <momohatt10@gmail.com>
2019-11-08 21:28:13 +0900

    Add testing environment for NonS2 program

--- test/Test.hs
+++ test/Test.hs
@@ -15,31 +15,96 @@ import           Test.HUnit
 
 import           Language.Egison
 import           Language.Egison.Core
-import           Language.Egison.Parser
+import qualified Language.Egison.Parser         as Parser
+import qualified Language.Egison.ParserNonS2    as ParserNonS2
 import           Language.Egison.Primitives
 import           Language.Egison.Types
 
+main :: IO ()
 main = do
-  (sampleTestCases, unitTestCases) <- partition (isSubsequenceOf "answer" . show) <$> glob "test/**/*.egi"
-  defaultMain $ hUnitTestToTests $ test $ map runUnitTestCase unitTestCases ++ map runSampleTestCase sampleTestCases
+  let unitTests     = map runUnitTestCase     unitTestCases
+      unitNonSTests = map runUnitTestCaseNonS unitNonSTestCases
+      sampleTests   = map runSampleTestCase   sampleTestCases
+   in defaultMain . hUnitTestToTests . test $ unitNonSTests ++ unitTests ++ sampleTests
+
+unitTestCases :: [FilePath]
+unitTestCases =
+  [ "test/syntax.egi"
+  , "test/primitive.egi"
+  , "test/lib/math/analysis.egi"
+  , "test/lib/math/tensor.egi"
+  , "test/lib/math/arithmetic.egi"
+  , "test/lib/math/algebra.egi"
+  , "test/lib/core/string.egi"
+  , "test/lib/core/base.egi"
+  , "test/lib/core/collection.egi"
+  , "test/lib/core/order.egi"
+  , "test/lib/core/number.egi"
+  ]
+
+unitNonSTestCases :: [FilePath]
+unitNonSTestCases =
+  [ "nons-test/test/syntax.egi"
+  , "nons-test/test/primitive.egi"
+  , "nons-test/test/lib/core/order.egi"
+  ]
+
+sampleTestCases :: [FilePath]
+sampleTestCases =
+  [ "test/answer/sample/graph.egi"
+  , "test/answer/sample/tree.egi"
+  , "test/answer/sample/one-minute-second.egi"
+  , "test/answer/sample/binary-counter.egi"
+  , "test/answer/sample/mickey.egi"
+  , "test/answer/sample/one-minute-first.egi"
+  , "test/answer/sample/poker-hands-with-joker.egi"
+  , "test/answer/sample/demo1.egi"
+  -- , "test/answer/sample/demo1-ja.egi"
+  , "test/answer/sample/primes.egi"
+  , "test/answer/sample/pi.egi"
+  , "test/answer/sample/mahjong.egi"
+  , "test/answer/sample/bipartite-graph.egi"
+  , "test/answer/sample/poker-hands.egi"
+  , "test/answer/sample/physics/tension3.egi"
+  , "test/answer/sample/physics/tension2.egi"
+  , "test/answer/sample/physics/tension.egi"
+  , "test/answer/sample/math/geometry/riemann-curvature-tensor-of-S2.egi"
+  , "test/answer/sample/math/number/17th-root-of-unity.egi"
+  , "test/answer/sample/math/number/5th-root-of-unity.egi"
+  , "test/answer/sample/math/number/sum-of-cubes.egi"
+  , "test/answer/sample/math/number/7th-root-of-unity-2.egi"
+  , "test/answer/sample/math/number/euler-totient-function.egi"
+  , "test/answer/sample/math/number/pi.egi"
+  , "test/answer/sample/math/number/napier.egi"
+  , "test/answer/sample/math/others/mobius-transformation.egi"
+  , "test/answer/sample/rosetta/consolidate.egi"
+  , "test/answer/sample/rosetta/lcs.egi"
+  , "test/answer/sample/rosetta/partial.egi"
+  ]
 
 runUnitTestCase :: FilePath -> Test
 runUnitTestCase file = TestLabel file . TestCase $ do
   env <- initialEnv defaultOption
   assertEgisonM $ do
-    exprs <- loadFile file
+    exprs <- Parser.loadFile file
     let (bindings, tests) = foldr collectDefsAndTests ([], []) exprs
     env' <- recursiveBind env bindings
     forM_ tests $ evalExprDeep env'
-      where
-        assertEgisonM :: EgisonM a -> Assertion
-        assertEgisonM m = fromEgisonM m >>= assertString . either show (const "")
+  where
+    assertEgisonM :: EgisonM a -> Assertion
+    assertEgisonM m = fromEgisonM m >>= assertString . either show (const "")
 
-        collectDefsAndTests (Define name expr) (bindings, tests) =
-          ((name, expr) : bindings, tests)
-        collectDefsAndTests (Test expr) (bindings, tests) =
-          (bindings, expr : tests)
-        collectDefsAndTests _ r = r
+runUnitTestCaseNonS :: FilePath -> Test
+runUnitTestCaseNonS file = TestLabel file . TestCase $ do
+  env <- initialEnv (defaultOption { optUseNonS2 = True })
+  assertEgisonM $ do
+    exprs <- ParserNonS2.loadFile file
+    let (bindings, tests) = foldr collectDefsAndTests ([], []) exprs
+    env' <- recursiveBind env bindings
+    forM_ tests $ evalExprDeep env'
+  where
+    assertEgisonM :: EgisonM a -> Assertion
+    assertEgisonM m = fromEgisonM m >>= assertString . either show (const "")
 
 runSampleTestCase :: FilePath -> Test
 runSampleTestCase file = TestLabel file . TestCase $ do
@@ -47,26 +112,26 @@ runSampleTestCase file = TestLabel file . TestCase $ do
   let directory_path = takeDirectory file
   answers <- readFile file
   assertEgisonM (lines answers) $ do
-    exprs <- loadFile (replaceDirectory file $ concat $ drop 2 $ splitPath directory_path)
+    exprs <- Parser.loadFile (replaceDirectory file $ concat $ drop 2 $ splitPath directory_path)
     let (bindings, tests) = foldr collectDefsAndTests ([], []) exprs
     env' <- recursiveBind env bindings
     vals <- forM tests (evalExprDeep env')
     return $ zip tests vals
-      where
-        assertEgisonM :: [String] -> EgisonM [(EgisonExpr, EgisonValue)] -> Assertion
-        assertEgisonM answers m = fromEgisonM m >>= assertString . either show (f answers)
+  where
+    assertEgisonM :: [String] -> EgisonM [(EgisonExpr, EgisonValue)] -> Assertion
+    assertEgisonM answers m = fromEgisonM m >>= assertString . either show (f answers)
 
-        collectDefsAndTests (Define name expr) (bindings, tests) =
-          ((name, expr) : bindings, tests)
-        collectDefsAndTests (Test expr) (bindings, tests) =
-          (bindings, expr : tests)
-        collectDefsAndTests _ r = r
+    f :: [String] -> [(EgisonExpr, EgisonValue)] -> String
+    f answers ls = g answers ls 0
+    g x y i = let (e, v) = unzip y in
+              if (x !! i) == show (v !! i)
+                 then (if i < (length y - 1) then g x y (i + 1)
+                                             else "")
+                 else "failed " ++ show (e !! i) ++ "\n expected: " ++ (x !! i) ++ "\n but found: " ++ show (v !! i)
 
-        f :: [String] -> [(EgisonExpr, EgisonValue)] -> String
-        f answers ls = g answers ls 0
-        g x y i = let (e, v) = unzip y in
-                  if (x !! i) == show (v !! i)
-                     then (if i < (length y - 1) then g x y (i + 1)
-                                                 else "")
-                     else "failed " ++ show (e !! i) ++ "\n expected: " ++ (x !! i) ++ "\n but found: " ++ show (v !! i)
-
+collectDefsAndTests :: EgisonTopExpr -> ([(Var, EgisonExpr)], [EgisonExpr]) -> ([(Var, EgisonExpr)], [EgisonExpr])
+collectDefsAndTests (Define name expr) (bindings, tests) =
+  ((name, expr) : bindings, tests)
+collectDefsAndTests (Test expr) (bindings, tests) =
+  (bindings, expr : tests)
+collectDefsAndTests _ r = r
