19412c6e7de629a8445558a0c287a570be64bda7
Satoshi Egi <egi@egison.org>
2019-11-11 08:24:51 +0900

    version 3.9.2

--- test/Test.hs
+++ test/Test.hs
@@ -44,10 +44,11 @@ unitTestCases =
 
 unitNonSTestCases :: [FilePath]
 unitNonSTestCases =
-  [ "nons-test/test/syntax.egi"
-  , "nons-test/test/primitive.egi"
-  , "nons-test/test/lib/core/base.egi"
-  , "nons-test/test/lib/core/order.egi"
+  [
+--    "nons-test/test/syntax.egi"
+--  , "nons-test/test/primitive.egi"
+--  , "nons-test/test/lib/core/base.egi"
+--  , "nons-test/test/lib/core/order.egi"
   ]
 
 sampleTestCases :: [FilePath]
