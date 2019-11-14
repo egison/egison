ec562194419a8846807696d512a825b87a89ec9d
momohatt <momohatt10@gmail.com>
2019-11-14 16:48:44 +0900

    [translator] Add list-oid expressions

--- hs-src/Language/Egison/PrettyPrint.hs
+++ hs-src/Language/Egison/PrettyPrint.hs
@@ -17,11 +17,13 @@ import           Data.Text.Prettyprint.Doc
 import           Language.Egison.Types
 
 prettyTopExprs :: [EgisonTopExpr] -> Doc [EgisonTopExpr]
-prettyTopExprs exprs = vsep (punctuate line (map pretty exprs))
+prettyTopExprs exprs = vsep $ punctuate line (map pretty exprs)
 
 instance Pretty EgisonTopExpr where
+  pretty (Define x (LambdaExpr args body)) =
+    pretty x <+> hsep (map pretty args) <+> equals <> softline <> pretty body
+  pretty (Define x expr) = pretty x <+> equals <> softline <> pretty expr -- TODO: use flatAlt
   pretty (Test expr) = pretty expr
-  -- pretty (Define x (LambdaExpr args body)) = pretty x <+> pretty args
 
 instance Pretty EgisonExpr where
   pretty (CharExpr x)    = squote <> pretty x <> squote
@@ -29,3 +31,26 @@ instance Pretty EgisonExpr where
   pretty (BoolExpr x)    = pretty x
   pretty (IntegerExpr x) = pretty x
   pretty (FloatExpr x)   = pretty x
+  pretty (VarExpr x)     = pretty x
+
+  pretty (TupleExpr xs) = tupled (map pretty xs)
+  pretty (CollectionExpr xs) = list (map pretty xs)
+  pretty (ArrayExpr xs)  = listoid "(|" "|)" (map pretty xs)
+  pretty (HashExpr xs)   = listoid "{|" "|}" (map (\(x, y) -> list [pretty x, pretty y]) xs)
+  pretty (VectorExpr xs) = listoid "[|" "|]" (map pretty xs)
+
+instance Pretty Arg where
+  pretty (ScalarArg x)         = pretty x
+  pretty (InvertedScalarArg x) = pretty "*$" <> pretty x
+  pretty (TensorArg x)         = pretty '%' <> pretty x
+
+instance Pretty Var where
+  -- TODO: indices
+  pretty (Var xs is) = concatWith (surround dot) (map pretty xs)
+
+instance Pretty InnerExpr where
+  pretty (ElementExpr x) = pretty x
+  pretty (SubCollectionExpr _) = error "Not supported"
+
+listoid :: String -> String -> [Doc ann] -> Doc ann
+listoid lp rp elems = encloseSep (pretty lp) (pretty rp) (comma <> space) elems
