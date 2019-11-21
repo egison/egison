d12a992e1ea61137a22b4b90da70e1a64bfa833f
momohatt <momohatt10@gmail.com>
2019-11-21 13:15:59 +0900

    [skip ci] Delete unused shell scripts

--- nons-test/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-TEST_DIR=nons-test/test
-find $TEST_DIR -type f -name "*.egi" | xargs -L 1 -t stack exec egison -- -t -N
-stack exec egison -- -t -N nons-test/loadpi.egi
-stack exec egison -- -t -N nons-test/new-tensor.egi
