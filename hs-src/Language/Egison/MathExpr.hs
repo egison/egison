57d5c7a5330c6cef2cf41ef9b691d81afa505ac3
Satoshi Egi <egi@egison.org>
2019-11-28 12:12:53 +0900

    split MathExpr.hs from Types.hs

--- hs-src/Language/Egison/MathExpr.hs
+++ hs-src/Language/Egison/MathExpr.hs
@@ -16,7 +16,7 @@ This module contains functions for mathematical expressions.
 module Language.Egison.MathExpr
     (
     -- * MathExpr Data
-      MathExpr (..)
+      ScalarData (..)
     , PolyExpr (..)
     , TermExpr (..)
     , SymbolExpr (..)
@@ -67,11 +67,14 @@ import           System.IO
 
 import           System.IO.Unsafe          (unsafePerformIO)
 
+import           Language.Egison.AST
+
 --
 -- Data
 --
 
-data MathExpr =
+
+data ScalarData =
     Div PolyExpr PolyExpr
  deriving (Eq)
 
@@ -82,15 +85,13 @@ data TermExpr =
     Term Integer [(SymbolExpr, Integer)]
 
 data SymbolExpr =
-    Symbol Id String [Index MathExpr]
-  | Apply EgisonValue [MathExpr]
-  | Quote MathExpr
-  | FunctionData (Maybe EgisonValue) [EgisonValue] [EgisonValue] [Index MathExpr] -- fnname argnames args indices
+    Symbol Id String [Index ScalarData]
+  | Apply ScalarData [ScalarData]
+  | Quote ScalarData
+  | FunctionData ScalarData [ScalarData] [ScalarData] [Index ScalarData] -- fnname argnames args indices
  deriving (Eq)
 
 type Id = String
-
-deriving instance Eq MathExpr
 
 instance Eq PolyExpr where
   (Plus []) == (Plus []) = True
@@ -122,22 +123,11 @@ instance Eq TermExpr where
                     Nothing -> False
   _ == _ = False
 
-deriving instance Eq SymbolExpr
-
 --
 -- Scalars
 --
 
-symbolMathExpr :: String -> String -> MathExpr
-symbolMathExpr id name = Div (Plus [Term 1 [(Symbol id name [], 1)]]) (Plus [Term 1 []])
-
-getSymId :: MathExpr -> String
-getSymId (Div (Plus [Term 1 [(Symbol id name [], 1)]]) (Plus [Term 1 []])) = id
-
-getSymName :: MathExpr -> String
-getSymName (Div (Plus [Term 1 [(Symbol id name [], 1)]]) (Plus [Term 1 []])) = name
-
-mathNormalize' :: MathExpr -> MathExpr
+mathNormalize' :: ScalarData -> ScalarData
 mathNormalize' mExpr = mathDivide (mathRemoveZero (mathFold (mathRemoveZeroSymbol mExpr)))
 
 termsGcd :: [TermExpr] -> TermExpr
@@ -161,7 +151,7 @@ termsGcd (t:ts) = f t ts
                          then (x, min n m)
                          else h (x, n) ys
 
-mathDivide :: MathExpr -> MathExpr
+mathDivide :: ScalarData -> ScalarData
 mathDivide (Div (Plus ts1) (Plus [])) = Div (Plus ts1) (Plus [])
 mathDivide (Div (Plus []) (Plus ts2)) = Div (Plus []) (Plus ts2)
 mathDivide (Div (Plus ts1) (Plus ts2)) =
@@ -193,7 +183,7 @@ mathDivideTerm (Term a xs) (Term b ys) =
     then (1, (x, n - m))
     else (1, (x, n))
 
-mathRemoveZeroSymbol :: MathExpr -> MathExpr
+mathRemoveZeroSymbol :: ScalarData -> ScalarData
 mathRemoveZeroSymbol (Div (Plus ts1) (Plus ts2)) =
   let p x = case x of
               (_, 0) -> False
@@ -202,7 +192,7 @@ mathRemoveZeroSymbol (Div (Plus ts1) (Plus ts2)) =
   let ts2' = map (\(Term a xs) -> Term a (filter p xs)) ts2 in
     Div (Plus ts1') (Plus ts2')
 
-mathRemoveZero :: MathExpr -> MathExpr
+mathRemoveZero :: ScalarData -> ScalarData
 mathRemoveZero (Div (Plus ts1) (Plus ts2)) =
   let ts1' = filter (\(Term a _) -> a /= 0) ts1 in
   let ts2' = filter (\(Term a _) -> a /= 0) ts2 in
@@ -210,11 +200,11 @@ mathRemoveZero (Div (Plus ts1) (Plus ts2)) =
       [] -> Div (Plus []) (Plus [Term 1 []])
       _  -> Div (Plus ts1') (Plus ts2')
 
-mathFold :: MathExpr -> MathExpr
+mathFold :: ScalarData -> ScalarData
 mathFold mExpr = mathTermFold (mathSymbolFold (mathTermFold mExpr))
 
 -- x^2 y x -> x^3 y
-mathSymbolFold :: MathExpr -> MathExpr
+mathSymbolFold :: ScalarData -> ScalarData
 mathSymbolFold (Div (Plus ts1) (Plus ts2)) = Div (Plus (map f ts1)) (Plus (map f ts2))
  where
   f :: TermExpr -> TermExpr
@@ -239,7 +229,7 @@ mathSymbolFold (Div (Plus ts1) (Plus ts2)) = Div (Plus (map f ts1)) (Plus (map f
                              else ((y, m), sgn)
 
 -- x^2 y + x^2 y -> 2 x^2 y
-mathTermFold :: MathExpr -> MathExpr
+mathTermFold :: ScalarData -> ScalarData
 mathTermFold (Div (Plus ts1) (Plus ts2)) = Div (Plus (f ts1)) (Plus (f ts2))
  where
   f :: [TermExpr] -> [TermExpr]
@@ -278,16 +268,16 @@ mathTermFold (Div (Plus ts1) (Plus ts2)) = Div (Plus (f ts1)) (Plus (f ts2))
 --  Arithmetic operations
 --
 
-mathPlus :: MathExpr -> MathExpr -> MathExpr
+mathPlus :: ScalarData -> ScalarData -> ScalarData
 mathPlus (Div m1 n1) (Div m2 n2) = mathNormalize' $ Div (mathPlusPoly (mathMultPoly m1 n2) (mathMultPoly m2 n1)) (mathMultPoly n1 n2)
 
 mathPlusPoly :: PolyExpr -> PolyExpr -> PolyExpr
 mathPlusPoly (Plus ts1) (Plus ts2) = Plus (ts1 ++ ts2)
 
-mathMult :: MathExpr -> MathExpr -> MathExpr
+mathMult :: ScalarData -> ScalarData -> ScalarData
 mathMult (Div m1 n1) (Div m2 n2) = mathNormalize' $ Div (mathMultPoly m1 m2) (mathMultPoly n1 n2)
 
-mathMult' :: MathExpr -> MathExpr -> MathExpr
+mathMult' :: ScalarData -> ScalarData -> ScalarData
 mathMult' (Div m1 n1) (Div m2 n2) = Div (mathMultPoly m1 m2) (mathMultPoly n1 n2)
 
 mathMultPoly :: PolyExpr -> PolyExpr -> PolyExpr
@@ -295,14 +285,14 @@ mathMultPoly (Plus []) (Plus _) = Plus []
 mathMultPoly (Plus _) (Plus []) = Plus []
 mathMultPoly (Plus ts1) (Plus ts2) = foldl mathPlusPoly (Plus []) (map (\(Term a xs) -> Plus (map (\(Term b ys) -> Term (a * b) (xs ++ ys)) ts2)) ts1)
 
-mathNegate :: MathExpr -> MathExpr
+mathNegate :: ScalarData -> ScalarData
 mathNegate (Div m n) = Div (mathNegate' m) n
 
 mathNegate' :: PolyExpr -> PolyExpr
 mathNegate' (Plus ts) = Plus (map (\(Term a xs) -> Term (negate a) xs) ts)
 
-mathNumerator :: MathExpr -> MathExpr
+mathNumerator :: ScalarData -> ScalarData
 mathNumerator (Div m _) = Div m (Plus [Term 1 []])
 
-mathDenominator :: MathExpr -> MathExpr
+mathDenominator :: ScalarData -> ScalarData
 mathDenominator (Div _ n) = Div n (Plus [Term 1 []])
