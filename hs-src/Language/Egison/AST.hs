0458d43bf819abd4953cdd7bdc7a34943ed5a1ed
momohatt <momohatt10@gmail.com>
2019-12-23 22:20:50 +0900

    [infix] Add Infix to EgisonTopExpr

--- hs-src/Language/Egison/AST.hs
+++ hs-src/Language/Egison/AST.hs
@@ -51,6 +51,7 @@ data EgisonTopExpr =
     -- temporary : we will replace load to import and export
   | LoadFile String
   | Load String
+  | Infix EgisonBinOp
  deriving (Show, Eq)
 
 data EgisonExpr =
