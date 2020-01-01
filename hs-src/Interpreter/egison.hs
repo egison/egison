808ec9d33ed01a9aa96d517250f3ccca596902a5
momohatt <momohatt10@gmail.com>
2020-01-01 12:37:21 +0900

    Update license year

--- hs-src/Interpreter/egison.hs
+++ hs-src/Interpreter/egison.hs
@@ -77,7 +77,7 @@ runWithOptions opts
 
 showBanner :: IO ()
 showBanner = do
-  putStrLn $ "Egison Version " ++ showVersion version ++ " (C) 2011-2019 Satoshi Egi"
+  putStrLn $ "Egison Version " ++ showVersion version ++ " (C) 2011-2020 Satoshi Egi"
   putStrLn $ "https://www.egison.org"
   putStrLn $ "Welcome to Egison Interpreter!"
 --  putStrLn $ "** Information **"
