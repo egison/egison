7d6094c15b1b693192600396f4a82609d2081e28
momohatt <momohatt10@gmail.com>
2020-03-02 16:46:25 +0900

    Sort lib names by alphabetical order

--- translate-lib.sh
+++ translate-lib.sh
@@ -3,14 +3,15 @@
 for f in lib/??*/??*
 do
     [ -d ${f} ] && continue
-    [ "${f}" = "lib/core/sexpr.segi" ] && continue
-    [ "${f}" = "lib/core/collection.segi" ] && continue
+    [ "${f}" = "lib/core/assoc.segi" ] && continue
     [ "${f}" = "lib/core/base.segi" ] && continue
+    [ "${f}" = "lib/core/collection.segi" ] && continue
+    [ "${f}" = "lib/core/io.segi" ] && continue
     [ "${f}" = "lib/core/maybe.segi" ] && continue
     [ "${f}" = "lib/core/number.segi" ] && continue
-    [ "${f}" = "lib/core/io.segi" ] && continue
     [ "${f}" = "lib/core/order.segi" ] && continue
     [ "${f}" = "lib/core/random.segi" ] && continue
+    [ "${f}" = "lib/core/string.segi" ] && continue
     echo ${f}
     stack exec -- egison-translate ${f} > "nons-"${f%.segi}.egi
 done
