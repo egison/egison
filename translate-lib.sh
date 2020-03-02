cb96d9e1e837fc697595a291d409a3219dc6bac4
momohatt <momohatt10@gmail.com>
2020-03-02 15:13:06 +0900

    Move capitalization to Parser.hs

--- translate-lib.sh
+++ translate-lib.sh
@@ -7,6 +7,8 @@ do
     [ "${f}" = "lib/core/collection.segi" ] && continue
     [ "${f}" = "lib/core/base.segi" ] && continue
     [ "${f}" = "lib/core/maybe.segi" ] && continue
+    [ "${f}" = "lib/core/number.segi" ] && continue
+    [ "${f}" = "lib/core/io.segi" ] && continue
     echo ${f}
     stack exec -- egison-translate ${f} > "nons-"${f%.segi}.egi
 done
