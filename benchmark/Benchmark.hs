e1efc92ad63e0cf86f6111a5b8d3ca598bebbc3b
momohatt <momohatt10@gmail.com>
2020-07-13 21:51:25 +0900

    Fix benchmark

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -1,20 +1,23 @@
-{-# LANGUAGE FlexibleInstances #-}
 module Main where
+
 import           Control.Applicative ((<$>), (<*>))
 import           Control.Applicative
-import           Control.DeepSeq     (NFData (rnf))
-import           Control.Monad.Except
 import           Criterion
 import           Criterion.Main
 import           Language.Egison
+import           Language.Egison.RState
 import           Language.Egison.CmdOptions
 
 runEgisonFile :: String -> IO ()
-runEgisonFile path = initialEnv defaultOption >>= flip (loadEgisonFile defaultOption) path >> return ()
+runEgisonFile path = evalRuntimeT defaultOption $ do
+  env <- initialEnv
+  _ <- loadEgisonFile env path
+  return ()
 
 main :: IO ()
 main = defaultMainWith defaultConfig
-         [ bgroup "fact" [ bench "30000" $ nfIO $ runEgisonFile "benchmark/fact-30000.egi" ]
+         [ bgroup "fact"
+           [ bench "30000" $ nfIO $ runEgisonFile "benchmark/fact-30000.egi" ]
          , bgroup "collection"
            [ bench "cons-bench" $ nfIO $ runEgisonFile "benchmark/collection-bench-cons.egi"
            , bench "cons-bench-large" $ nfIO $ runEgisonFile "benchmark/collection-bench-cons-large.egi"
