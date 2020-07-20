3209c14834ab9f3813d9e13329a43eaef1bb4854
momohatt <momohatt10@gmail.com>
2020-07-20 16:24:36 +0900

    Rewrite mathSymbolFold with sweet-egison

--- hs-src/Language/Egison/Data.hs
+++ hs-src/Language/Egison/Data.hs
@@ -76,8 +76,7 @@ import           Text.Show.Unicode         (ushow)
 import           Data.Ratio
 import           System.IO
 
-import           Control.Egison            (consM, joinM, joinConsM, nilM,
-                                            cons, join, joinCons, nil,
+import           Control.Egison            (joinConsM, nilM, joinCons, nil,
                                             bfs, match, mc, List(..))
 import qualified Control.Egison            as M
 
