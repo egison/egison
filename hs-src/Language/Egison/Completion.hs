644a9b8bb303d930bfe3a77dd88001d907a69b34
momohatt <momohatt10@gmail.com>
2020-08-13 16:37:26 +0900

    Suggest all defined words in completion

--- hs-src/Language/Egison/Completion.hs
+++ hs-src/Language/Egison/Completion.hs
@@ -9,51 +9,33 @@ module Language.Egison.Completion
   ( completeEgison
   ) where
 
+import           Data.HashMap.Strict        (keys)
 import           Data.List
+import           System.Console.Haskeline    hiding (catch, handle, throwTo)
 
-import           System.Console.Haskeline   hiding (catch, handle, throwTo)
+import           Language.Egison.Data        (Env (..))
+import           Language.Egison.Parser.NonS (upperReservedWords, lowerReservedWords)
 
 -- |Complete Egison keywords
-completeEgison :: Monad m => CompletionFunc m
-completeEgison arg@(')':_, _) = completeParen arg
-completeEgison arg@('>':_, _) = completeParen arg
-completeEgison arg@(']':_, _) = completeParen arg
-completeEgison arg@('}':_, _) = completeParen arg
-completeEgison arg@('(':_, _) = completeWord Nothing " \t<>[]{}$," completeAfterOpenParen arg
-completeEgison arg@('<':_, _) = completeWord Nothing " \t()[]{}$," completeAfterOpenCons arg
-completeEgison arg@(' ':_, _) = completeWord Nothing "" completeNothing arg
-completeEgison arg@('[':_, _) = completeWord Nothing "" completeNothing arg
-completeEgison arg@('{':_, _) = completeWord Nothing "" completeNothing arg
-completeEgison arg@([], _) = completeWord Nothing "" completeNothing arg
-completeEgison arg@(_, _) = completeWord Nothing " \t[]{}$," completeEgisonKeyword arg
-
-completeAfterOpenParen :: Monad m => String -> m [Completion]
-completeAfterOpenParen str = return $ map (\kwd -> Completion kwd kwd False) $ filter (isPrefixOf str) $ egisonPrimitivesAfterOpenParen ++ egisonKeywordsAfterOpenParen
-
-completeAfterOpenCons :: Monad m => String -> m [Completion]
-completeAfterOpenCons str = return $ map (\kwd -> Completion kwd kwd False) $ filter (isPrefixOf str) egisonKeywordsAfterOpenCons
+completeEgison :: Monad m => Env -> CompletionFunc m
+completeEgison _   arg@(')':_, _) = completeParen arg
+completeEgison _   arg@(']':_, _) = completeParen arg
+completeEgison _   arg@('(':_, _) = completeWord Nothing "" completeNothing arg
+completeEgison _   arg@(' ':_, _) = completeWord Nothing "" completeNothing arg
+completeEgison _   arg@('[':_, _) = completeWord Nothing "" completeNothing arg
+completeEgison _   arg@([], _)    = completeWord Nothing "" completeNothing arg
+completeEgison env arg@(_, _)     = completeWord Nothing " \t[]{}$," (completeEgisonKeyword env) arg
 
 completeNothing :: Monad m => String -> m [Completion]
 completeNothing _ = return []
 
-completeEgisonKeyword :: Monad m => String -> m [Completion]
-completeEgisonKeyword str = return $ map (\kwd -> Completion kwd kwd False) $ filter (isPrefixOf str) egisonKeywords
-
-egisonPrimitivesAfterOpenParen :: [String]
-egisonPrimitivesAfterOpenParen = map ((:) '(') ["+", "-", "*", "/", "numerator", "denominator", "modulo", "quotient", "remainder", "neg", "abs", "eq?", "lt?", "lte?", "gt?", "gte?", "round", "floor", "ceiling", "truncate", "sqrt", "exp", "log", "sin", "cos", "tan", "asin", "acos", "atan", "sinh", "cosh", "tanh", "asinh", "acosh", "atanh", "itof", "rtof", "stoi", "read", "show", "empty?", "uncons", "unsnoc", "assert", "assert-equal"]
-
-egisonKeywordsAfterOpenParen :: [String]
-egisonKeywordsAfterOpenParen = map ((:) '(') ["define", "let", "letrec", "lambda", "match", "match-all", "match-lambda", "matcher", "algebraic-data-matcher", "pattern-function", "if", "loop", "io", "do"]
-                            ++ ["id", "or", "and", "not", "char", "eq?/m", "compose", "compose3", "list", "map", "between", "repeat1", "repeat", "filter", "separate", "concat", "foldr", "foldl", "map2", "zip", "member?", "member?/m", "include?", "include?/m", "any", "all", "length", "count", "count/m", "car", "cdr", "rac", "rdc", "nth", "take", "drop", "while", "reverse", "multiset", "add", "add/m", "delete-first", "delete-first/m", "delete", "delete/m", "difference", "difference/m", "union", "union/m", "intersect", "intersect/m", "set", "unique", "unique/m", "print", "print-to-port", "each", "pure-rand", "fib", "fact", "divisor?", "gcd", "primes", "find-factor", "prime-factorization", "p-f", "min", "max", "min-and-max", "power", "mod", "sort", "intersperse", "intercalate", "split", "split/m"]
-
-egisonKeywordsAfterOpenCons :: [String]
-egisonKeywordsAfterOpenCons = map ((:) '<') ["nil", "cons", "join", "snoc", "nioj"]
-
-egisonKeywordsInNeutral :: [String]
-egisonKeywordsInNeutral = "something" : ["bool", "string", "integer", "nats", "primes"]
+completeEgisonKeyword :: Monad m => Env -> String -> m [Completion]
+completeEgisonKeyword (Env env _) str = do
+  let definedWords = map show $ concatMap keys env
+  return $ map (\kwd -> Completion kwd kwd False) $ filter (isPrefixOf str) (egisonKeywords ++ definedWords)
 
 egisonKeywords :: [String]
-egisonKeywords = egisonPrimitivesAfterOpenParen ++ egisonKeywordsAfterOpenParen ++ egisonKeywordsAfterOpenCons ++ egisonKeywordsInNeutral
+egisonKeywords = upperReservedWords ++ lowerReservedWords
 
 completeParen :: Monad m => CompletionFunc m
 completeParen (lstr, _) = case closeParen lstr of
@@ -82,15 +64,9 @@ removeCharAndStringLiteral' (_:str)         = removeCharAndStringLiteral' str
 closeParen' :: Integer -> String -> Maybe String
 closeParen' _ []        = Nothing
 closeParen' 0 ('(':_)   = Just ")"
-closeParen' 0 ('<':_)   = Just ">"
 closeParen' 0 ('[':_)   = Just "]"
-closeParen' 0 ('{':_)   = Just "}"
 closeParen' n ('(':str) = closeParen' (n - 1) str
-closeParen' n ('<':str) = closeParen' (n - 1) str
 closeParen' n ('[':str) = closeParen' (n - 1) str
-closeParen' n ('{':str) = closeParen' (n - 1) str
 closeParen' n (')':str) = closeParen' (n + 1) str
-closeParen' n ('>':str) = closeParen' (n + 1) str
 closeParen' n (']':str) = closeParen' (n + 1) str
-closeParen' n ('}':str) = closeParen' (n + 1) str
 closeParen' n (_:str)   = closeParen' n str
