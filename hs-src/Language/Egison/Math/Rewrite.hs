3068659e12cc7372a509c358b93adbed7c666b4e
Satoshi Egi <egi@egison.org>
2020-12-01 12:00:15 +0900

    remove comment-outed old code

--- hs-src/Language/Egison/Math/Rewrite.hs
+++ hs-src/Language/Egison/Math/Rewrite.hs
@@ -167,13 +167,6 @@ rewriteSqrt = mapTerms' f
                  in case (n' * m', Term n' x', Term m' y') of
                       (1, Term _ [], Term _ []) -> mathMult (SingleTerm c z) (SingleTerm a xss)
                       (_, _, _) -> mathMult (SingleTerm c z) (SingleTerm a ((makeApply "sqrt" [SingleTerm (n' * m') (x' ++ y')], 1) : xss)) |]
---                 sqrtxy = (if x' == [] then [] else [(makeApply "sqrt" [SingleTerm 1 x'], 1)]) ++
---                            (if y' == [] then [] else [(makeApply "sqrt" [SingleTerm 1 y'], 1)])
---              in mathMult
---                   (SingleTerm c z)
---                   (SingleTerm
---                     a
---                     ((makeApply "sqrt" [SingleTerm (n' * m') []], 1) : sqrtxy ++ xss)) |]
       , [mc| _ -> SingleTerm a xs |]
       ]
 
