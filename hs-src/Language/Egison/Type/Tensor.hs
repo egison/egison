4f54579eabc2380f87834011ca735e052d6bc456
Satoshi Egi <egi@egison.org>
2026-01-09 08:15:21 +0900

    implement normalize tensor type

--- hs-src/Language/Egison/Type/Tensor.hs
+++ hs-src/Language/Egison/Type/Tensor.hs
@@ -13,8 +13,20 @@ module Language.Egison.Type.Tensor
 import           Language.Egison.Type.Types
 
 -- | Normalize tensor types
--- According to type-tensor-simple.md, Tensor MathExpr unifies with MathExpr
--- So Tensor a can unify with a (scalar)
+-- For example,
+-- Tensor a -> Tensor a
+-- Tensor (Tensor a) -> Tensor a
+-- Tensor (Tensor (Tensor a)) -> Tensor a
+-- [Tensor (Tensor a)] -> [Tensor a]
 normalizeTensorType :: Type -> Type
-normalizeTensorType t = t
-
+normalizeTensorType (TTensor (TTensor t)) = normalizeTensorType (TTensor t)
+normalizeTensorType (TTensor t) = TTensor (normalizeTensorType t)
+normalizeTensorType (TTuple ts) = TTuple (map normalizeTensorType ts)
+normalizeTensorType (TCollection t) = TCollection (normalizeTensorType t)
+normalizeTensorType (TInductive name ts) = TInductive name (map normalizeTensorType ts)
+normalizeTensorType (THash k v) = THash (normalizeTensorType k) (normalizeTensorType v)
+normalizeTensorType (TMatcher t) = TMatcher (normalizeTensorType t)
+normalizeTensorType (TFun a r) = TFun (normalizeTensorType a) (normalizeTensorType r)
+normalizeTensorType (TIO t) = TIO (normalizeTensorType t)
+normalizeTensorType (TIORef t) = TIORef (normalizeTensorType t)
+normalizeTensorType t = t  -- TInt, TFloat, TBool, TChar, TString, TVar, TAny
